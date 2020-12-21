09859c88161ea8d7f743f61813933ad510a8cf54
gudkgau <gudmundur.gautason@gmail.com>
2020-12-21 15:39:38 +0000

    Búinn að laga þannig að verkefni virki

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,4 +4,4 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images')
-    url = models.URLField(blank=True) \\hérna má reiturinn vera tómur
+    url = models.URLField(blank=True)
