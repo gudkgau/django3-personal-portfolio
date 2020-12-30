0c34b4f25493c0355026a6d25c7b3ea72c153328
gudkgau <gudmundur.gautason@gmail.com>
2020-12-30 00:32:54 +0000

    First

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+#Hérnaer ég að sækja Blog Class úr models og skrá hann inn í admin
+from .models import Blog
+
+#Hérna er ég að registera þetta inn í admin
+admin.site.register(Blog)
+# Register your models here.
 # Register your models here.
